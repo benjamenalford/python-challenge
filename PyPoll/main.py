8367606447054e86a8d94f1f38da1f6ebc05f5f9
Benjamen Alford <benjamen.alford@gmail.com>
2024-03-30 12:07:37 -0500

    finished almost

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,10 @@
 import csv
 # Needed:
-# 1. The total number of votes cast - done
+# 1. The total number of votes cast - done *
 # 2. A complete list of candidates who received votes - done*
 # 3. The percentage of votes each candidate won - done*
 # 4. The total number of votes each candidate won - done *
-# 5. The winner of the election based on popular vote
+# 5. The winner of the election based on popular vote - done *
 
 # start here
 # create variables
@@ -32,6 +32,16 @@ with open(election_file_path) as election_file:
         else:  # candidate is in list
             candidate_id = candidates.index(candidate)
             candidate_votes[candidate_id] += 1
+# done reading the file
+
+# find the winner
+winning_candidate = ""
+winning_candidate_votes = 0
+for candidate in candidates:
+    current_votes = candidate_votes[candidates.index(candidate)]
+    if current_votes > winning_candidate_votes:
+        winning_candidate = candidate
+        winning_candidate_votes = current_votes
 
 # print the results to screen
 print('Election Results')
@@ -42,11 +52,11 @@ for candidate in candidates:
     current_candidate_votes = candidate_votes[candidates.index(candidate)]
     current_vote_pct = (current_candidate_votes / total_votes) * 100
     print(
-        f'{candidate}: {round(current_vote_pct,3)}% ({candidate_votes})')
+        f'{candidate}: {round(current_vote_pct,3)}% ({current_candidate_votes})')
+print('-------------------------')
+print(f'Winner: {winning_candidate}')
+print('-------------------------')
 
-print(len(candidates), "candidates")
-print(candidates, "candidates")
-print(candidate_votes)
 # print the results to file
 
 
