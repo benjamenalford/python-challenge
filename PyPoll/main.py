41dd7397c4cd0ab1e8d38f5206da3e3102243061
Benjamen Alford <benjamen.alford@gmail.com>
2024-03-30 12:14:46 -0500

    Finished PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,17 +1,11 @@
 import csv
-# Needed:
-# 1. The total number of votes cast - done *
-# 2. A complete list of candidates who received votes - done*
-# 3. The percentage of votes each candidate won - done*
-# 4. The total number of votes each candidate won - done *
-# 5. The winner of the election based on popular vote - done *
 
-# start here
 # create variables
 election_file_path = "PyPoll\Resources\election_data.csv"
 total_votes = 0
 candidates = []
 candidate_votes = []
+
 # open the file
 with open(election_file_path) as election_file:
     csv_file = csv.reader(election_file)
@@ -58,18 +52,18 @@ print(f'Winner: {winning_candidate}')
 print('-------------------------')
 
 # print the results to file
-
-
+out_file_path = "PyPoll\election_results.txt"
+with open(out_file_path, 'w') as file_out:
+    file_out.write('Election Results\n')
+    file_out.write('-------------------------\n')
+    file_out.write(f'Total Votes: {total_votes}\n')
+    file_out.write('-------------------------\n')
+    for candidate in candidates:
+        current_candidate_votes = candidate_votes[candidates.index(candidate)]
+        current_vote_pct = (current_candidate_votes / total_votes) * 100
+        file_out.write(
+            f'{candidate}: {round(current_vote_pct,3)}% ({current_candidate_votes})\n')
+    file_out.write('-------------------------\n')
+    file_out.write(f'Winner: {winning_candidate}\n')
+    file_out.write('-------------------------\n')
 # end here
-
-# example output
-# Election Results
-# -------------------------
-# Total Votes: 369711
-# -------------------------
-# Charles Casper Stockham: 23.049% (85213)
-# Diana DeGette: 73.812% (272892)
-# Raymon Anthony Doane: 3.139% (11606)
-# -------------------------
-# Winner: Diana DeGette
-# -------------------------
